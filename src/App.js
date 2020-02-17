1825710b467a48c9316599030c2aca888b6d2de0
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-17 08:55:57 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import uuid from 'uuid'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const friends = [
+  { id: uuid(), fname: 'Jane', lname: 'doe', married: false },
+  { id: uuid(), fname: 'john', lname: 'smith', married: false },
+]
+const initialState = {
+  formValues: {
+    fname: '',
+    lname: '',
+  },
+  friends,
+  favFriendId: friends[0].id
 }
 
-export default App;
+export default function App() {
+  return (
+    <div className="App">
+      {/* here we can add a new friend */}
+      <h4>Add a new friend (married defaults to false)</h4>
+      <form className='form' onSubmit={Function.prototype}>
+        <label>first name
+        <input
+            value=''
+            onChange={Function.prototype}
+            name='fname'
+          />
+        </label><br />
+
+        <label>last name
+        <input
+            value=''
+            onChange={Function.prototype}
+            name='lname'
+          />
+        </label><br />
+
+        <input type='submit' />
+      </form>
+
+      {/* list of current friends */}
+      <h4>My friends:</h4>
+      {
+        initialState.friends.map(fr => (
+          <div
+            key={fr.id}
+          >
+            {fr.fname} {fr.lname} is{fr.married ? ' ' : ' NOT '}married
+              <button onClick={Function.prototype}>Mark Married</button>
+            <button onClick={Function.prototype}>Fav</button>
+          </div>
+        ))
+      }
+
+      {/* my current favorite friend */}
+      <h4 className='fav'>My fav friend is {
+        initialState.friends
+          .find(fr => fr.id === initialState.favFriendId)
+          .fname
+      }</h4>
+    </div>
+  )
+}
